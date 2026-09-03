251070df95a7ba6d9ad61a0d4febb6cad91b2e89
wfn <fangnuowu@gmail.com>
2026-09-03 16:12:53 +0800

    docs: add site favicon

--- website/en/docs/conf.py
+++ website/en/docs/conf.py
@@ -14,6 +14,7 @@ language = "en"
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
+html_favicon = "../../favicon.png"
 html_theme_options = {
     "collapse_navigation": False,
     "navigation_depth": 4,
--- website/zh/docs/conf.py
+++ website/zh/docs/conf.py
@@ -22,6 +22,7 @@ language = "zh_CN"
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
+html_favicon = "../../favicon.png"
 
 html_theme_options = {
     # RTD defaults already feel close to NCCL; keep minimal overrides
